3168028596e10bb428645fb1b4f4a02163a05a57
VTB4060778 <dvstroganov@dev.vtb.ru>
2022-09-19 10:44:23 +0300

    release
    Former-commit-id: 05761b01e78fb3efda43926f4ef7c945d7efcedc

--- app/version.sh
+++ app/version.sh
@@ -5,28 +5,27 @@ html_dir="/app"
 html_file="index.html"
 oc_dir="/app"
 
-> $html_dir/$html_file
-
 # собираем HTML
 ## выбираем стиль 
-echo "<style>
-	/* Стили таблицы (IKSWEB) */
-	table.iksweb{text-decoration: none;border-collapse:collapse;width:100%;text-align:center;}
-	table.iksweb th{font-weight:normal;font-size:14px; color:#ffffff;background-color:#354251;}
-	table.iksweb td{font-size:13px;color:#354251;}
-	table.iksweb td,table.iksweb th{white-space:pre-wrap;padding:10px 5px;line-height:13px;vertical-align: middle;border: 1px solid #354251;}	table.iksweb tr:hover{background-color:#f9fafb}
-	table.iksweb tr:hover td{color:#354251;cursor:default;}
-</style>"  >> $html_dir/$html_file
+html="<style>"
+html="${html}/* Стили таблицы (IKSWEB) */"
+html="${html}table.iksweb{text-decoration: none;border-collapse:collapse;width:100%;text-align:center;}"
+html="${html}table.iksweb th{font-weight:normal;font-size:14px; color:#ffffff;background-color:#354251;}"
+html="${html}table.iksweb td{font-size:13px;color:#354251;}"
+html="${html}table.iksweb td,table.iksweb th{white-space:pre-wrap;padding:10px 5px;line-height:13px;vertical-align: middle;border: 1px solid #354251;}	table.iksweb tr:hover{background-color:#f9fafb}"
+html="${html}table.iksweb tr:hover td{color:#354251;cursor:default;}"
+html="${html}</style>"
 
 ## получаем текущий namespace в OC и текущую даты
-namespace=$(${oc_dir}/oc project | grep -oP '"(.*?)" ')
+namespace=$(oc project | grep -oP '"(.*?)" ')
 date="$(date +'%Y-%m-%d %H:%M:%S') UTC"
 
 ## запрос данных из опеншифт
-html=$(${oc_dir}/oc get pods -o jsonpath="{range .items[*]}{'<tr>'}{'<td align="left">'}{.metadata.labels.app}{'</td>'}{'<td align="left">'}{.spec.containers[0].image}{'</td>'}{'<td>'}{.spec.containers[0].ports[0].containerPort}{'</td>'}{'<td>'}{.status.phase}{'</td>'}{'</tr>'}{end}")
+table=$(oc get pods -o jsonpath="{range .items[*]}{'<tr>'}{'<td align="left">'}{.metadata.labels.app}{'</td>'}{'<td align="left">'}{.spec.containers[0].image}{'</td>'}{'<td>'}{.spec.containers[0].ports[0].containerPort}{'</td>'}{'<td>'}{.status.phase}{'</td>'}{'</tr>'}{end}")
 ## добавляем время и HTML разметку и заголовок таблицы
-html="<html><body><p>update time: ${date}<br>namespace: ${namespace}</p><br><table class='iksweb'><thead><tr><th>NAME</th><th>VERSION</th><th>PORTS</th><th>STATUS</th></tr></thead>${html}</table></html>"
+html="${html}<html><body><p>update time: ${date}<br>namespace: ${namespace}</p><br><table class='iksweb'><thead><tr><th>NAME</th><th>VERSION</th><th>PORTS</th><th>STATUS</th></tr></thead>${table}</table></html>"
 
 # выгружаем html
+> $html_dir/$html_file
 echo $html >> $html_dir/$html_file
 

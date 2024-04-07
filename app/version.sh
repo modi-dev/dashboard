1c1e0e6b2f972b83af1edf4255537d2aa89e5373
Sergey Baier <sbaier@vtb.ru>
2024-04-07 17:23:48 +0300

    Update dashboard
    Former-commit-id: ec2368fc1198d8752c41f0d3b59e2e37977df7cd

--- app/version.sh
+++ app/version.sh
@@ -11,7 +11,7 @@ html="<style>"
 html="${html}/* Стили таблицы (IKSWEB) */"
 html="${html}table.iksweb{text-decoration: none;border-collapse:collapse;width:100%;text-align:center;}"
 html="${html}table.iksweb th{font-weight:normal;font-size:14px; color:#ffffff;background-color:#354251;}"
-html="${html}table.iksweb td{font-size:13px;color:#354251;}"
+html="${html}table.iksweb td{font-size:14px;color:#354251;}"
 html="${html}table.iksweb td,table.iksweb th{white-space:pre-wrap;padding:10px 5px;line-height:13px;vertical-align: middle;border: 1px solid #354251;}  table.iksweb tr:hover{background-color:#f9fafb}"
 html="${html}table.iksweb tr:hover td{color:#354251;cursor:default;}"
 html="${html}</style>"
@@ -21,40 +21,18 @@ namespace=$(${oc_dir}/oc get pods -o jsonpath="{.items[0].metadata.namespace}")
 date="$(date +'%Y-%m-%d %H:%M:%S') UTC"
 
 ## запрос данных из опеншифт
-table=$(${oc_dir}/oc get pods -o jsonpath="
+table=$(${oc_dir}/oc get deploy -o jsonpath="
 {range .items[?(@.metadata.labels.app)]}
-    {'<tr>'}
-        {'<td align="left">'}
-            {.metadata.labels.app}
-        {'</td>'}
-        {'<td align="left">'}
-            {.spec.containers[?(@.name=='main')].image}
-        {'</td>'}
-        {'<td>'}
-            {.status.phase}
-        {'</td>'}
-        {'<td>'}
-            {.metadata.creationTimestamp}{'</br>'}
-        {'</td>'}
-        {'<td align="left">'}
-            {range .spec.containers[*]}
-                {'<b>'}{@.name}{'</b>'}{' port: '}
-                {range .ports[0]}
-                    {.containerPort}
-                {end}
-                {'</br>'}
-                {'req.cpu: '}{.resources.requests.cpu}{'</br>'}
-                {'req.mem: '}{.resources.requests.memory}{'</br>'}
-                {'lim.cpu: '}{.resources.limits.cpu}{'</br>'}
-                {'lim.mem: '}{.resources.limits.memory}{'</br>'}{'</br>'}
-            {end}
-        {'</td>'}
-    {'</tr>'}
+{'<tr>'}
+    {'<td align="left">'}{.metadata.labels.app}{'</td>'}{'<td align="left">'}{..containers[?(@.name == 'main')].image}{\" \"}{'</td>'}{'<td>'}{..metadata.annotations.ms\-branch}{\" \"}{'</td>'}{'<td>'}{..metadata.annotations.config\-branch}{\" \"}{'</td>'}{'<td>'}{.status.conditions[?(@.reason == 'NewReplicaSetAvailable')].lastUpdateTime}{\" \"}{'</br>'}{'</td>'}{'<td align="left">'}{..containers[?(@.name == 'main')].ports[0].containerPort} {'</td>'} {'<td align="left">'}{'CPU: '}{..containers[?(@.name == 'main')].resources.requests.cpu}{'</br>'}{'</br>'}{'RAM: '} {..containers[?(@.name == 'main')].resources.requests.memory}{'</td>'} {'<td align="left">'} <!--replicas-->{.spec.replicas} {'</td>'}
+{'</tr>'}
 {end}
-")
+" | grep -vE istio\|dashbord\|stub\|devtool\|hello\|devtool\|kafka\|tyk\|mrp\|\<!--replicas--\>0)
+
+sed_table=$(echo $table | sed 's/nexus[^:]*://g')
 
 ## добавляем время, HTML разметку и заголовок таблицы
-html="${html}<html><body><p>update time: ${date}<br>namespace: ${namespace}</p><br><table class='iksweb'><thead><tr><th>NAME</th><th>VERSION</th><th>STATUS</th><th>CREATION DATE</th><th>LIMITS & PORTS</th></tr></thead>${table}</table></html>"
+html="${html}<html><body><p style="font-size:12px" > update time: ${date}<br>namespace: ${namespace}</p><br><table class='iksweb'><thead><tr><th>NAME</th><th>VERSION</th><th>MsBranch</th><th>ConfigBranch</th><th>CREATION DATE</th><th>PORT</th><th>REQUEST</th><th>REPLICAS</th></tr></thead>${sed_table}</table></html>"
 
 # выгружаем html
 > $html_dir/$html_file
